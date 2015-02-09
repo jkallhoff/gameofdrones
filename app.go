d7fe9d75ab96b638094e5adf7230ef623e388ac3
Jesse Kallhoff <jkallhoff@gmail.com>
2015-02-09 15:16:50 -0600

    Better intelligence. 37th on the leader board

--- app.go
+++ app.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 import "math"
 import "sort"
+import "os"
 
-//import "os"
 //fmt.Fprintf(os.Stderr, "Distance: %d, CLOSEST: %d\n",distance, closestDistance)
 
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+
+const radius = 100.0
 
 func main() {
 	// P: number of players in the game (2 to 4 players)
@@ -25,26 +27,67 @@ func main() {
 	for {
 		game.LoadRound()
 
-		for _, drone := range game.Me().Drones {
+		for di, drone := range game.Me().Drones {
 			closestZones := drone.ClosestZones(game.Zones)
+			currentZone := drone.CurrentZone
+			var targetZone *Zone
 
-			if drone.TargetZone == nil {
-				drone.TargetZone = closestZones[0].Zone
-				drone.Move()
-			}
+			if currentZone != nil {
+				fmt.Fprintf(os.Stderr, "Current Zone Isn't Empty: %d", di)
+				currentEnemyShipCount := currentZone.MaxEnemyShips(game.Players)
+				currentMyShipCount := currentZone.MyShips
 
-			if drone.TargetZone != nil {
-				if drone.AtZone != true {
-					drone.Move()
+				//WE CONTROL THE CURRENT ZONE
+				if currentZone.ControllerId == game.MeId {
+					fmt.Fprintf(os.Stderr, "And I control the current zone: %d", di)
+					switch {
+					case currentEnemyShipCount > 0 && (currentMyShipCount-currentEnemyShipCount) <= 1:
+						targetZone = currentZone
+						break
+					case currentEnemyShipCount == 0 || (currentMyShipCount-currentEnemyShipCount) > 1:
+						targetZone = closestZones.ClosestOpenEmpty(drone, game)
+						if targetZone == nil {
+							targetZone = closestZones.ClosestPush(drone, game)
+							if targetZone == nil {
+								targetZone = closestZones.ClosestTippingPoint(drone, game)
+							}
+						}
+						break
+					}
 				} else {
-					drone.Move()
+					//ENEMY CONTROLS CURRENT NODE
+					fmt.Fprintf(os.Stderr, "And I don't own the current zone: %d", di)
+					if targetZone == nil {
+						targetZone = closestZones.ClosestPush(drone, game)
+						if targetZone == nil {
+							targetZone = closestZones.ClosestTippingPoint(drone, game)
+						}
+					}
+				}
 
+			} else {
+				fmt.Fprintf(os.Stderr, "Current Zone is empty: %d", di)
+				targetZone = closestZones.ClosestOpenEmpty(drone, game)
+				if targetZone == nil {
+					targetZone = closestZones.ClosestPush(drone, game)
+					if targetZone == nil {
+						targetZone = closestZones.ClosestTippingPoint(drone, game)
+					}
 				}
 			}
-			//closestZones := d.ClosestZones(game.Zones)
-			//d.MoveTo(closestZones[0].Zone.Center)
+
+			if targetZone == nil {
+				targetZone = closestZones.ClosestTippingPoint(drone, game)
+			}
+
+			drone.TargetZone = targetZone
+
+			if drone.TargetZone != nil {
+				drone.Move()
+			}
 		}
 	}
+
 }
 
 //POINT TYPES
@@ -62,14 +105,34 @@ func (start *Point) DistanceTo(end *Point) float64 {
 
 //ZONE TYPES
 type Zone struct {
-	Center        *Point
-	ControllerId  int
-	MaxEnemyShips int
+	Center       *Point
+	ControllerId int
+	//MaxEnemyShips int
+	MyShips int
 }
 
 type ZoneDistance struct {
 	Zone     *Zone
 	Distance float64
+}
+
+func (zone *Zone) MaxEnemyShips(players Players) int {
+	var maxCount int
+	maxCount = 0
+
+	for _, p := range players {
+		playerCount := 0
+		for _, d := range p.Drones {
+			if d.CurrentZone == zone {
+				playerCount++
+			}
+		}
+		if playerCount > maxCount {
+			maxCount = playerCount
+		}
+	}
+
+	return maxCount
 }
 
 type Zones []*Zone
@@ -79,15 +142,55 @@ func (a ZoneDistances) Len() int           { return len(a) }
 func (a ZoneDistances) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ZoneDistances) Less(i, j int) bool { return a[i].Distance < a[j].Distance }
 
+func (distances ZoneDistances) ClosestOpenEmpty(drone *Drone, game *Game) *Zone {
+	for _, distance := range distances {
+		if distance.Zone.ControllerId != game.MeId && distance.Zone.MaxEnemyShips(game.Players) == 0 {
+			return distance.Zone
+			break
+		}
+	}
+
+	return nil
+}
+
+func (distances ZoneDistances) ClosestPush(drone *Drone, game *Game) *Zone {
+	for _, distance := range distances {
+		if distance.Zone.ControllerId != game.MeId && distance.Zone.MaxEnemyShips(game.Players)-distance.Zone.MyShips == 1 {
+			return distance.Zone
+			break
+		}
+	}
+	return nil
+}
+
+func (distances ZoneDistances) ClosestTippingPoint(drone *Drone, game *Game) *Zone {
+	var returnDifference int
+	returnDifference = 10000
+	var targetZone *Zone
+
+	for _, distance := range distances {
+		if distance.Zone.ControllerId != game.MeId {
+			difference := distance.Zone.MaxEnemyShips(game.Players) - distance.Zone.MyShips
+
+			if difference < returnDifference {
+				returnDifference = difference
+				targetZone = distance.Zone
+			}
+		}
+	}
+
+	return targetZone
+}
+
 //DRONE TYPES
 type Drone struct {
-	Location   *Point
-	TargetZone *Zone
-	AtZone     bool
+	Location    *Point
+	TargetZone  *Zone
+	CurrentZone *Zone
 }
 
 func (drone *Drone) Move() {
-	fmt.Printf("%d %d\n", drone.TargetZone.Center.X, drone.TargetZone.Center.Y)
+	drone.MoveTo(drone.TargetZone.Center)
 }
 
 func (drone *Drone) MoveTo(point *Point) {
@@ -105,15 +208,22 @@ func (drone *Drone) ClosestZones(zones Zones) ZoneDistances {
 
 	sort.Sort(returnDistances)
 
-	if drone.TargetZone != nil {
-		if returnDistances[0].Zone == drone.TargetZone && returnDistances[0].Distance <= 99 {
-			drone.AtZone = true
-		} else {
-			drone.AtZone = false
+	return returnDistances
+}
+
+func (drone *Drone) SetCurrentZone(zones Zones) bool {
+	for _, z := range zones {
+		distance := drone.Location.DistanceTo(z.Center)
+
+		if distance <= radius {
+			drone.CurrentZone = z
+			//fmt.Fprintf(os.Stderr, "SETTING CURRENT ZONE:",drone.CurrentZone)
+			return true
 		}
 	}
 
-	return returnDistances
+	drone.CurrentZone = nil
+	return false
 }
 
 type Drones []*Drone
@@ -178,11 +288,20 @@ func SetupGame(p, id, d, z int) *Game {
 func (game *Game) LoadRound() {
 	for _, zv := range game.Zones {
 		fmt.Scan(&zv.ControllerId)
+		zv.MyShips = 0
+		//zv.MaxEnemyShips = 0
 	}
 
-	for _, pv := range game.Players {
+	for pi, pv := range game.Players {
 		for _, dv := range pv.Drones {
 			fmt.Scan(&dv.Location.X, &dv.Location.Y)
+			if dv.SetCurrentZone(game.Zones) {
+				if dv.CurrentZone.ControllerId == pi {
+					if game.MeId == pi {
+						dv.CurrentZone.MyShips++
+					}
+				}
+			}
 		}
 	}
 }
